b0db41d55099e8520133b665b65cc51f4984950b
tanakarei <>
2012-08-04 12:48:45 +0700

    version 0.01

---
+++ web-edc/src/com/republic/data/DocumentX.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.republic.data;
+
+/**
+ *
+ * @author Panuchart_bun
+ */
+public class DocumentX {
+    
+}
